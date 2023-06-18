7f3934a316915df50d6a2ff846f02a90301f49ad
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-06-18 19:21:49 +0900

    update logo

--- src/components/project/projects.js
+++ src/components/project/projects.js
@@ -4,7 +4,7 @@ import Project from "@/components/project/project";
 
 import Smart from '../../../json/project/smart.json'
 import Portfolio from '../../../json/project/portfolio.json'
-import Benefit from '../../../json/project/suitable-benefit.json'
+import Benefit from '../../../json/project/need.json'
 
 export default function Projects() {
     return (
